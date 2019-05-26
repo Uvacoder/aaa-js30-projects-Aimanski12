60cbea26723b932fa81f6626552eceaf4e808a5a
Leo espinoza <leo.spinoza@yahoo.com>
2019-05-26 17:24:45 -0500

    :sunglasses: making it better

---
+++ build/precache-manifest.fae32a658db2915d16b304f6402e3eed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f858f0d0b2757543a19f5c045a3f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d208af27d3dabae4a5c",
+    "url": "/static/css/main.217cc1b7.chunk.css"
+  },
+  {
+    "revision": "b0497fc2462238b1cb86",
+    "url": "/static/js/2.3d83c83e.chunk.js"
+  },
+  {
+    "revision": "5d208af27d3dabae4a5c",
+    "url": "/static/js/main.1d3231c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
