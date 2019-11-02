13e2ea31b33b399aa470e5c8a1c016e7dadb91d4
Leo espinoza <leo.spinoza@yahoo.com>
2019-11-01 21:49:39 -0500

    :sunglasses: added documentation

--- src/assests/configSetup.js
+++
@@ -1,7 +0,0 @@
-export const api = 'f79c1f33b89b8f1e137114c46a4df913'
-const baseURl = `https://api.themoviedb.org/3`
-
-export const mountURL = `${baseURl}/trending/movie/day?api_key=${api}`
-export const changeURL = `${baseURl}/search/movie?query=`
-export const fallBackURL = `${baseURl}/trending/tv/week?api_key=${api}`
-export const genreURL = `${baseURl}/genre/movie/list?language=en-US&api_key=${api}`
